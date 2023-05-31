a6f1b0c63ab18028eb88e10e1936503fa9d62ecc
Vivek <tailorvivekgec19@gmail.com>
2023-05-31 09:09:12 +0530

    random user

--- lib/random_user/screen/homeScreen.dart
+++ lib/random_user/screen/homeScreen.dart
@@ -53,17 +53,37 @@ class _HomeScreenState extends State<HomeScreen> {
                 UserModel user = snapshot.data!;
 
                 return Center(
-                  child: Column(
-                    children: [
-                      Text("${user.results![0].name!.title}. ${user.results![0].name!.first} ${user.results![0].name!.last}",style: TextStyle(fontSize: 16.sp),),
-                      Text("${user.results![0].gender}".toUpperCase()),
-                      Padding(
-                        padding: const EdgeInsets.all(15),
-                        child: ClipOval(child: Image.network("${user.results![0].picture!.large}",height: 200,width: 200,fit: BoxFit.fill,),),
-                      ),
-                      Text("${user.info!.version}")
+                  child: Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            ClipOval(child: Image.network("${user.results![0].picture!.large}",height: 100,width: 100,fit: BoxFit.fill,),),
+                            SizedBox(width: 10,),
+                            Column(crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("${user.results![0].name!.title}. ${user.results![0].name!.first} ${user.results![0].name!.last}",style: TextStyle(fontSize: 16.sp),),
+                                Text("${user.results![0].gender}".toUpperCase()),
+                              ],
+                            ),
+                          ],
+                        ),
 
-                    ],
+
+
+                        Tile("Id","${user.results![0].id!.name}-${user.results![0].id!.value}"),
+                        Tile("Mobile No.", "${user.results![0].phone}"),
+                        Tile("Home Contact No.", "${user.results![0].cell}"),
+                        Tile("Date of Birth", "${user.results![0].dob!.date}"),
+                        Tile("AGE", "${user.results![0].dob!.age}"),
+                        Tile("Address", "${user.results![0].location!.street!.number}, ${user.results![0].location!.street!.name} - ${user.results![0].location!.postcode}"),
+                        Tile("Origin", "${user.results![0].location!.city}, ${user.results![0].location!.state}, ${user.results![0].location!.country}"),
+
+
+
+                      ],
+                    ),
                   ),
                 );
               }
@@ -72,6 +92,18 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
 
 
+      ),
+    );
+  }
+  Widget Tile(String? title, String? data)
+  {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text("$title :",style: TextStyle(fontWeight: FontWeight.w600,fontSize: 15),),
+          Text("$data",style: TextStyle(fontWeight: FontWeight.w300,),),
+        ],
       ),
     );
   }
